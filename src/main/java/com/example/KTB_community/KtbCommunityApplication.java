6783f3307392fff8b1f3cfcca2b77e3fc5bccc53
SUHYUN YU <suhyun1219yu@gmail.com>
2026-06-22 10:36:01 +0900

    초기커밋 분리

---
+++ src/main/java/com/example/KTB_community/KtbCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.KTB_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KTB_community/KtbCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KTB_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
